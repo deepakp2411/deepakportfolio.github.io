f67fd42d45024c1d143618ea0ddc743bf8621569
Deepak <deepakpatel@gmail.com>
2022-04-26 22:19:10 +0530

    creatd new design

--- index.js
+++
@@ -1,7 +0,0 @@
-var typed = document.querySelector(".auto-input");
-typed = new Typed(".auto-input",{
-    strings: ["Full stack Devloper","React Js Developer","Freelancer"],
-    typeSpeed: 100,
-    backspeed: 100,
-    loop: true
-});
