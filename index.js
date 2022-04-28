efe892231edf7aabdf64c22372ecb07fd13cf605
Deepak <deepakpatel@gmail.com>
2022-04-28 22:39:41 +0530

    new effects

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = document.querySelector(".auto-input");
+typed = new Typed(".auto-type",{
+    strings: ["Full stack Devloper","React Js Developer","Freelancer"],
+    typeSpeed: 200,
+    backspeed: 200,
+    loop: true
+});
