dc9f287a3b45892e6787893d4156533e4637ddeb
Deepak <deepakpatel@gmail.com>
2022-05-01 15:41:33 +0530

    changes

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// var typed = document.querySelector(".auto-input");
-// typed = new Typed(".auto-type",{
-//     strings: ["Full stack Devloper","React Js Developer","Freelancer"],
-//     typeSpeed: 200,
-//     backspeed: 200,
-//     loop: true
-// });
-console.log("hello");
 
 const navMenu = document.getElementById('nav-menu'),
 toggleMenu = document.getElementById('nav-toggle'),
